f4505f83167524a88f151ba64937c404ff982588
HiImNew123 <139051354+HiImNew123@users.noreply.github.com>
2023-11-29 09:11:11 +0800

    Add files via upload

---
+++ expocalc.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ExpoCalc</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+<body>
+    
+    <h1>EXPOCALC</h1>
+    
+    
+</body>
+</html>
