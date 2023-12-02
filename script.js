aedfc0f1686f8a05218a2884831f5631ac1d234e
HiImNew123 <139051354+HiImNew123@users.noreply.github.com>
2023-12-02 18:44:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quadratic Equation Solver</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Quadratic Equation Solver Tutorial</h1>
-        <label for="a">Enter coefficient a:</label>
-        <input type="number" id="a" placeholder="Enter a">
+var currentStep = 0;
 
-        <label for="b">Enter coefficient b:</label>
-        <input type="number" id="b" placeholder="Enter b">
+function solveQuadratic() {
+    // Get coefficients from input fields
+    var a = parseFloat(document.getElementById("a").value);
+    var b = parseFloat(document.getElementById("b").value);
+    var c = parseFloat(document.getElementById("c").value);
 
-        <label for="c">Enter coefficient c:</label>
-        <input type="number" id="c" placeholder="Enter c">
+    // Calculate discriminant
+    var discriminant = b * b - 4 * a * c;
 
-        <button onclick="nextStep()">Next Step</button>
+    // Step-by-step solution
+    var steps = [];
 
-        <div id="result"></div>
-    </div>
+    if (discriminant > 0) {
+        var root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        var root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 
-    <script src="script.js"></script>
-</body>
-</html>
+        // Step 1
+        steps.push("Calculate Discriminant: b^2 - 4ac = " + discriminant);
+
+        // Step 2
+        steps.push("Roots are real and different.");
+        steps.push("Use the quadratic formula: (-b ± √(b^2 - 4ac)) / 2a");
+
+        // Step 3
+        steps.push("Root 1 = (-" + b + " + √" + discriminant + ") / " + (2 * a) + " = " + root1);
+
+        // Step 4
+        steps.push("Root 2 = (-" + b + " - √" + discriminant + ") / " + (2 * a) + " = " + root2);
+    } else if (discriminant == 0) {
+        var root = -b / (2 * a);
+
+        // Step 1
+        steps.push("Calculate Discriminant: b^2 - 4ac = " + discriminant);
+
+        // Step 2
+        steps.push("Roots are real and same.");
+        steps.push("Use the quadratic formula: (-b ± √(b^2 - 4ac)) / 2a");
+
+        // Step 3
+        steps.push("Root = -" + b + " / " + (2 * a) + " = " + root);
+    } else {
+        var realPart = -b / (2 * a);
+        var imaginaryPart = Math.sqrt(Math.abs(discriminant)) / (2 * a);
+
+        // Step 1
+        steps.push("Calculate Discriminant: b^2 - 4ac = " + discriminant);
+
+        // Step 2
+        steps.push("Roots are complex and different.");
+        steps.push("Use the quadratic formula: (-b ± √(b^2 - 4ac)) / 2a");
+
+        // Step 3
+        steps.push("Real part = -" + b + " / " + (2 * a) + " = " + realPart);
+
+        // Step 4
+        steps.push("Imaginary part = √" + Math.abs(discriminant) + " / " + (2 * a) + " = " + imaginaryPart);
+
+        // Step 5
+        steps.push("Root 1 = " + realPart + " + " + imaginaryPart + "i");
+        steps.push("Root 2 = " + realPart + " - " + imaginaryPart + "i");
+    }
+
+    // Display steps
+    displayResult(steps[currentStep]);
+}
+
+function nextStep() {
+    currentStep++;
+    solveQuadratic();
+
+    // If all steps are displayed, disable the button
+    if (currentStep === steps.length - 1) {
+        document.querySelector("button").disabled = true;
+    }
+}
+
+function displayResult(message) {
+    document.getElementById("result").innerHTML = message;
+}
